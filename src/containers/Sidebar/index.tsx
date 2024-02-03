fd91ee451f6e7a7369f6e3a3d1b4bda706148d13
EmersonPenelli <emersonpenelli@gmail.com>
2024-02-03 15:44:10 -0300

    Projeto finalizado

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,27 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
+
+type Props = {
+  trocaTema: () => void
+}
+
+const Sidebar = (props: Props) => (
+  <aside>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Emerson Penelli</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        emersonpenelli
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro de Software
+      </Descricao>
+      <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
+    </SidebarContainer>
+  </aside>
+)
+
+export default Sidebar
